09b03347746e289cd0488e019da97dfb38c00868
Hitesh <hiteshkumarpattanatyak03@gmail.com>
2020-03-26 12:10:33 +0530

    changed compress to compression and added a dummy line in exception file

--- startup/prod.js
+++ startup/prod.js
@@ -1,5 +1,6 @@
 const helmet = require('helmet');
-const compress = require('compress');
+// const compress = require('compress');
+const compress = require('compression');
 
 module.exports = function(app) {
   app.use(helmet());
